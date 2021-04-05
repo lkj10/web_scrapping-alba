2f4a7cb7a3104d187cc7275d29e8944a6178ba14
lkj10 <lkj10@users.noreply.github.com>
2021-04-05 13:11:40 +0900

    not finished main.py
    cuz Alba ban me ...
    maybe I cause a lot of traffic.....
    
    I can fix it later!

--- main.py
+++ main.py
@@ -1,17 +1,57 @@
-import os
 import csv
 import requests
 from bs4 import BeautifulSoup
-
-os.system("clear")
+from save_to_file import save_to_file
 
 alba_url = "http://www.alba.co.kr"
 
+site_list = []
+dict = {}
 
 codes_request = requests.get(alba_url)
 codes_soup = BeautifulSoup(codes_request.text, "html.parser")
 
 tables = codes_soup.find("div", {"class": "goodsLogo goodsMain"})
+
+product_tag_ul = tables.find('ul', {"class": "goodsBox"})
+
+product_tag_li = product_tag_ul.find_all('li')
+
+for i in product_tag_li:
+    temp = i.find('a')['href']
+    if temp[-4:] == 'MAIN':
+        site_list.append(f"{alba_url}{i.find('a')['href']}")
+
+
 companys = tables.find_all("span", {"class": "company"})
-print(companys)
-# print(tables)
+
+for company in companys:
+    try:
+        file = open(f"{company.string}.csv", mode="w", encoding='utf-8')
+        writer = csv.writer(file)
+        writer.writerow(["place", "title", "time", "pay", "date"])
+    except:
+        pass
+
+for site in site_list:
+    codes_request = requests.get(site)
+    codes_soup = BeautifulSoup(codes_request.text, "html.parser")
+    div = codes_soup.find(
+        "div", {"class": "goodsList detailViewList curation detail-list--likeCorp"})
+    place = div.find("table").find("tbody").find_all(
+        "td", {"class": "local first"})
+    title = div.find("table").find("tbody").find_all(
+        "td", {"class": "title"})
+    time = div.find("table").find("tbody").find_all(
+        "td", {"class": "time"})
+    pay = div.find("table").find("tbody").find_all(
+        "td", {"class": "pay"})
+    date = div.find("table").find("tbody").find_all(
+        "td", {"class": "date"})
+    dict['place'] = place
+    dict['title'] = title
+    dict['time'] = time
+    dict['pay'] = pay
+    dict['date'] = date
+    for dic in dict:
+        writer.writerow(list(dict.values()))
---
+++ save_to_file.py
@@ -0,0 +1,11 @@
+
+import csv
+
+
+def save_to_file(jobs):
+    file = open("jobs.csv", mode="w", encoding='utf-8')
+    writer = csv.writer(file)
+    writer.writerow(["place", "title", "time", "pay", "date"])
+    for job in jobs:
+        writer.writerow(list(job.values()))
+    return
