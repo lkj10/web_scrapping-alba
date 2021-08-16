24bd29f900721ee918154764c6ea515ef8e60e05
lkj10 <lkj10@users.noreply.github.com>
2021-08-16 17:50:49 +0900

    feat: main.py completed!
    옛날에 실패했던 코드 올바르게 수정.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
-from save_to_file import save_to_file
 
 alba_url = "http://www.alba.co.kr"
 
@@ -11,47 +10,43 @@ dict = {}
 codes_request = requests.get(alba_url)
 codes_soup = BeautifulSoup(codes_request.text, "html.parser")
 
-tables = codes_soup.find("div", {"class": "goodsLogo goodsMain"})
+tables = codes_soup.find("div", {"id": "MainSuperBrand"})
 
 product_tag_ul = tables.find('ul', {"class": "goodsBox"})
 
-product_tag_li = product_tag_ul.find_all('li')
 
-for i in product_tag_li:
-    temp = i.find('a')['href']
-    if temp[-4:] == 'MAIN':
-        site_list.append(f"{alba_url}{i.find('a')['href']}")
+product_tag_li = product_tag_ul.find_all(
+    'a', {"class": "goodsBox-info"})
 
 
-companys = tables.find_all("span", {"class": "company"})
-
-for company in companys:
+for a in product_tag_li:
+    table = a.find("span", {"class": "company"})
     try:
-        file = open(f"{company.string}.csv", mode="w", encoding='utf-8')
+        file = open(f"{table.string}.csv", mode="w",
+                    encoding='utf-8-sig', newline='')
         writer = csv.writer(file)
         writer.writerow(["place", "title", "time", "pay", "date"])
     except:
         pass
 
-for site in site_list:
-    codes_request = requests.get(site)
-    codes_soup = BeautifulSoup(codes_request.text, "html.parser")
-    div = codes_soup.find(
-        "div", {"class": "goodsList detailViewList curation detail-list--likeCorp"})
-    place = div.find("table").find("tbody").find_all(
-        "td", {"class": "local first"})
-    title = div.find("table").find("tbody").find_all(
-        "td", {"class": "title"})
-    time = div.find("table").find("tbody").find_all(
-        "td", {"class": "time"})
-    pay = div.find("table").find("tbody").find_all(
-        "td", {"class": "pay"})
-    date = div.find("table").find("tbody").find_all(
-        "td", {"class": "date"})
-    dict['place'] = place
-    dict['title'] = title
-    dict['time'] = time
-    dict['pay'] = pay
-    dict['date'] = date
-    for dic in dict:
+    site = a.attrs['href']
+    site_request = requests.get(site)
+    site_soup = BeautifulSoup(site_request.text, "html.parser")
+    div = site_soup.find("div", {"class": "goodsList goodsJob"})
+    table_tbody = div.find("table").find("tbody")
+    place = table_tbody.find_all("td", {"class": "local first"})
+    title = table_tbody.find_all("td", {"class": "title"})
+    time = table_tbody.find_all("td", {"class": "data"})
+    pay = table_tbody.find_all("td", {"class": "pay"})
+    date = table_tbody.find_all("td", {"class": "regDate last"})
+
+    for i in range(len(place)):
+        dict['place'] = place[i].text
+        dict['title'] = (title[i].find(
+            "span", {"class": "company"})).string
+        dict['time'] = time[i].string
+        dict['pay'] = pay[i].text
+        dict['date'] = date[i].text
         writer.writerow(list(dict.values()))
+    dict = {}
+print("완료")
